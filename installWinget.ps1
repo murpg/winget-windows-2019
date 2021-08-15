417ba3d30d20e02774f0ce3097619dab71a4d6bc
George Murphy <georgemurphy@websbygeorge.com>
2021-08-15 18:34:13 -0400

    updated powershell script so it works.

--- installWinget.ps1
+++ installWinget.ps1
@@ -1,5 +1,6 @@
 #Install NtObjectManager module
 Set-PackageSource -Name PSGallery -Trusted
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module NtObjectManager -Force
 
 cd C:\Users\dbgAdmin
@@ -7,17 +8,32 @@ mkdir Temp
 cd Temp
 
 # Source file location
-$source = 'https://github.com/murpg/winget-windows-2019/blob/master/appx.zip'
+$source1 = 'https://github.com/murpg/winget-windows-2019/raw/master/appx/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
 # Destination to save the file
-$destination = 'C:\Users\dbgAdmin\Temp\appx.zip'
+$destination1 = 'C:\Users\dbgAdmin\Temp\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
 #Download the file
-Invoke-WebRequest -Uri $source -OutFile $destination
-Expand-Archive -Path appx.zip -DestinationPath C:\Users\dbgAdmin\Temp
+Invoke-WebRequest -Uri $source1 -OutFile $destination1
 
+$source2 = 'https://github.com/murpg/winget-windows-2019/raw/master/appx/Microsoft.VCLibs.140.00.UWPDesktop_8wekyb3d8bbwe.appx'
+# Destination to save the file
+$destination2 = 'C:\Users\dbgAdmin\Temp\Microsoft.VCLibs.140.00.UWPDesktop_8wekyb3d8bbwe.appx'
+#Download the file
+Invoke-WebRequest -Uri $source2 -OutFile $destination2
+
+$source3 = 'https://github.com/murpg/winget-windows-2019/raw/master/appx/Microsoft.VCLibs.140.00_8wekyb3d8bbwe.appx'
+# Destination to save the file
+$destination3 = 'C:\Users\dbgAdmin\Temp\Microsoft.VCLibs.140.00_8wekyb3d8bbwe.appx'
+#Download the file
+Invoke-WebRequest -Uri $source3 -OutFile $destination3
 
 Add-AppxPackage -Path .\Microsoft.VCLibs.140.00.UWPDesktop_8wekyb3d8bbwe.appx
 Add-AppxPackage -Path .\Microsoft.VCLibs.140.00_8wekyb3d8bbwe.appx
 Add-AppxPackage -Path .\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -ForceUpdateFromAnyVersion
+
+# Create reparse point 
+$installationPath = (Get-AppxPackage Microsoft.DesktopAppInstaller).InstallLocation
+Set-ExecutionAlias -Path "C:\Windows\System32\winget.exe" -PackageName "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe" -EntryPoint "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe!winget" -Target "$installationPath\AppInstallerCLI.exe" -AppType Desktop -Version 3
+explorer.exe "shell:appsFolder\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe!winget"
 
 # Create reparse point 
 $installationPath = (Get-AppxPackage Microsoft.DesktopAppInstaller).InstallLocation
